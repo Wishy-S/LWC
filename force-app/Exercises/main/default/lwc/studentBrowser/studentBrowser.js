ae0db636883a91c4b2b8aa0a338ba8ff047ad0b0
Wishy-S <mk2371972@gmail.com>
2022-08-09 15:41:14 +0530

    Exercise 2-9 Contacts queried

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -2,7 +2,14 @@ import { LightningElement ,wire} from 'lwc';
 import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
 export default class StudentBrowser extends LightningElement {
     
+    selectedDeliveryId = '';    
+    selectedInstructorId = '';
     
-    @wire(getStudents,{instructorId:'',courseDelivery:''})students;
-    
+    @wire(getStudents, { instructorId: '$selectedInstructorId',
+    courseDeliveryId: '$selectedDeliveryId' })
+    students;
+    handleFilterChange(event){
+        this.selectedDeliveryId = event.detail.deliveryId;
+        this.selectedInstructorId = event.detail.instructorId;
+    }   
 }
--- force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
+++ force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
@@ -43,8 +43,22 @@ export default class StudentBrowserForm extends LightningElement {
             this.error = error;
             }
         }
-        onInstructorChange(event) {
+        onInstructorChange(event){
             this.selectedDeliveryId = '';
             this.selectedInstructorId = event.target.value;
+            this.notifyParent( );
+        }
+        onDeliveryChange(event) {
+            this.selectedDeliveryId = event.target.value;
+            this.notifyParent() ;
+        }
+        notifyParent() {
+            const evt = new CustomEvent('filterchange', {
+            detail: {
+            instructorId: this.selectedInstructorId,
+            deliveryId: this.selectedDeliveryId,
+            }
+            });
+            this.dispatchEvent(evt);
         }
 }
