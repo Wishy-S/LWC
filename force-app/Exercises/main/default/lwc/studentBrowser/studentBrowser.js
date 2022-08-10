f89700f90a40a13ecf9d1e064e8ac3555bc09d9f
Wishy-S <mk2371972@gmail.com>
2022-08-10 16:52:23 +0530

    Exercise 5-2 Updates StudentDetail

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -58,4 +58,8 @@ export default class StudentBrowser extends NavigationMixin(LightningElement) {
             }
             });
         }
+        handleRowClick(event) {
+            const studentId = event.detail.pk;
+            this.updateSelectedStudent(studentId);
+        }
 }
